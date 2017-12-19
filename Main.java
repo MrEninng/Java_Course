88a8de8048727be77e65826b58209a75dd4d4aea
Alexandr Dubov <eninng@hotmail.com>
2017-12-19 11:16:33 +0300

    Add tests for vectors methods

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import vectors.*;
 
 public class Main {
-    public static void main(String... s) {
+    public static void main(String... s) throws VectorsExceptions.IncompatibleVectorSizesException {
         ArrayVector myVector = new ArrayVector(6);
         System.out.println("GetSize: " + myVector.getSize());
         myVector.setElement(0, 8.2);
@@ -24,7 +25,7 @@ public class Main {
         System.out.println("getMax: " + myVector.getMax());
         System.out.println("getMin: " + myVector.getMin());
 
-        ArrayVector myVector2 = ArrayVector.mult(myVector, 5);
+        IVector myVector2 = SVectors.mult(myVector, 5);
         System.out.println("MmVector2: myVector MULT 5");
         for (int i = 0; i < myVector2.getSize(); ++i) {
             System.out.println(myVector2.getElement(i) + " ");
@@ -34,18 +35,61 @@ public class Main {
         ArrayVector myShortVector = new ArrayVector(2);
         myShortVector.setElement(0, 3.4);
         myShortVector.setElement(1, 112.7);
-        ArrayVector myVector3 = ArrayVector.sum(myVector2, myShortVector);
+        IVector myVector3 = SVectors.sum(myVector2, myShortVector);
         System.out.println("Array after SUM:");
         for (int i = 0; i < myVector3.getSize(); ++i) {
             System.out.println(myVector3.getElement(i) + " ");
         }
 
-        ArrayVector myVector4 = ArrayVector.scalarMult(myVector3, myShortVector);
+        IVector myVector4 = SVectors.scalarMult(myVector3, myShortVector);
         System.out.println("Array after scalarMult:");
         for (int i = 0; i < myVector4.getSize(); ++i) {
             System.out.println(myVector4.getElement(i) + " ");
         }
 
+        System.out.println("\n*\n*\n*\nLinkedListVector\n*\n*\n*\n");
+
+        LinkedListVector lVector1 = new LinkedListVector();
+        lVector1.addElement(5.3);
+        lVector1.addElement(1);
+        lVector1.addElement(29.23);
+        lVector1.addElement(72.0);
+
+        for (int i = 0; i < lVector1.getSize(); ++i) {
+            System.out.print(lVector1.getElement(i) + " ");
+        }
+
+        System.out.println("\nNorm: " + lVector1.getNorm());
+
+        lVector1.deleteElement(0);
+
+        lVector1.setElement(0, 3.2);
+
+        for (int i = 0; i < lVector1.getSize(); ++i) {
+            System.out.print(lVector1.getElement(i) + " ");
+        }
+
+        System.out.println("\nMult: ");
+        IVector tmp = SVectors.mult(lVector1, 6);
+        for (int i = 0; i < tmp.getSize(); ++i) {
+            System.out.print(tmp.getElement(i) + " ");
+        }
+
+        System.out.println("\nScalar Mult: ");
+        tmp = SVectors.scalarMult(lVector1, myVector);
+        for (int i = 0; i < tmp.getSize(); ++i) {
+            System.out.print(tmp.getElement(i) + " ");
+        }
+
+        System.out.println("\nMyVector ");
+        for (int i = 0; i < myVector.getSize(); ++i) {
+            System.out.print(myVector.getElement(i) + " ");
+        }
+        System.out.println("\nSum: ");
+        tmp = SVectors.sum(lVector1, myVector);
+        for (int i = 0; i < tmp.getSize(); ++i) {
+            System.out.print(tmp.getElement(i) + " ");
+        }
     }
-    
+
 }
